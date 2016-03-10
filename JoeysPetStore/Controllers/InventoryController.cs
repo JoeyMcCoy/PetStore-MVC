633e4b1b736696515299899584ad3a11ecc1c3a5
Joey McCoy <joey_mccoy@live.com>
2016-03-10 17:17:10 -0500

    Committing source code

---
+++ JoeysPetStore/Controllers/InventoryController.cs
@@ -0,0 +1,35 @@
+﻿using JoeysPetStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoeysPetStore.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private PetDBContext db = new PetDBContext();
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View(db.Pets.ToList());
+        }
+        // GET: /Inventory/Details
+       
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pets.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+    }
+}
---
+++ JoeysPetStore/Models/Pet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace JoeysPetStore.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateRecieved { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }  
+     
+    }
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
